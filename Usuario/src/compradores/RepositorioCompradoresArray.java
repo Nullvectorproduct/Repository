3dfab27dece6dfd82b98e65bc5a4e817e57df80d
Nullvectorproduct <48688103+Nullvectorproduct@users.noreply.github.com>
2019-06-03 09:50:55 -0300

    Repositorio Lista Completo
    Falta apenas a classe de Cadastros.

--- Usuario/src/compradores/RepositorioCompradoresArray.java
+++ Usuario/src/compradores/RepositorioCompradoresArray.java
@@ -8,7 +8,7 @@ public class RepositorioCompradoresArray implements RepositorioCompradoresInterf
 		this.compradores = new Comprador[200];
 		this.indice = 0;
 	}
-
+	//Metodo existe retorna um boolean que  apenas confirma a existencia ou nao do comprador procurado no repositorio, por isso não lança exceção.
 	public boolean existe(Comprador comprador) {
 		for (int i = 0; i < 200; i++) {
 			if (this.compradores[i] != null) {
@@ -18,8 +18,8 @@ public class RepositorioCompradoresArray implements RepositorioCompradoresInterf
 			}
 		}
 		return false;
-	} 
-
+	}
+	//Inserir irá inserir os compradores no array de Comprador. Não lança exceção, pois nele é possível ter vários compradores iguais.
 	public void inserir(Comprador comprador) {
 		if (indice < 199) {
 			if (compradores[indice] == null) {
@@ -36,9 +36,9 @@ public class RepositorioCompradoresArray implements RepositorioCompradoresInterf
 			// compradores= compradores2;
 			// }
 	}
-
+	//Remover irá retirar o comprador do array, ao substituir com null e mover todos após else uma posição abaixo. 
 	public void remover(Comprador comprador) throws CompradorNaoEncontradoException {
-		for (int i = 0; i < 200; i++) {
+		for (int i = 0; i < 198; i++) {
 			if (this.compradores[i].getCPF().equals(comprador.getCPF())) {
 				for (int n = 0; n < indice; n++) {
 					compradores[n] = compradores[n + 1];
@@ -48,7 +48,7 @@ public class RepositorioCompradoresArray implements RepositorioCompradoresInterf
 		}
 		throw new CompradorNaoEncontradoException();
 	}
-
+	//Metodo procurar retorna um Comprador, aquele que foi procurado.
 	public Comprador procurar(String cpf) throws CompradorNaoEncontradoException {
 		for (int i = 0; i < 200; i++) {
 			if (this.compradores[i].getCPF().equals(cpf)) {
@@ -57,7 +57,7 @@ public class RepositorioCompradoresArray implements RepositorioCompradoresInterf
 		}
 		throw new CompradorNaoEncontradoException();
 	}
-
+	//Atualizar altera os atributos de um determinado Comprador do array, recebendo assim um novo comprador.
 	public void atualizar(Comprador comprador) throws CompradorNaoEncontradoException {
 		for (int i = 0; i < 200; i++) {
 			if (this.compradores[i].getCPF().equals(comprador.getCPF())) {
